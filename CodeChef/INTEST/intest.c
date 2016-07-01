66e5bc96909b9db522c099309536cdf2185e5db6
NIDHI vaghasiya <NIDHI vaghasiya>
2016-07-01 13:36:53 +0530

    Intest

---
+++ CodeChef/INTEST/intest.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	long long int a;
+	int i,d,c=0;
+	scanf("%d %d",&i,&d);
+	while(i--){
+	         scanf("%lld",&a);
+	         if(a%d==0) c++;
+	}
+	printf("%d",c);
+	return 0;
+}
+
